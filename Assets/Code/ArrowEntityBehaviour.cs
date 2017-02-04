4d7c92d318df005880c85438aaa95591d7aa75e9
angelickite <angelickite@gmail.com>
2017-02-04 15:55:58 +0100

    initial commit

---
+++ Assets/Code/ArrowEntityBehaviour.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowEntityBehaviour : MonoBehaviour
+{
+    [HideInInspector]
+    public float errorDuration;
+
+    [HideInInspector]
+    public float errorSize;
+
+    [HideInInspector]
+    public float successDuration;
+
+    [HideInInspector]
+    public float successSize;
+
+    [HideInInspector]
+    public float fadeAwayDuration;
+
+    [HideInInspector]
+    public Image image;
+
+    [HideInInspector]
+    public BattleArrowKeySystem system;
+
+    private Behaviour behaviour = Behaviour.NONE;
+    private float t;
+    private float remainingMoveDistance;
+    private Vector2 moveTarget;
+
+    public void ActError()
+    {
+        behaviour = Behaviour.ERROR;
+        t = 0;
+    }
+
+    public void ActSuccess()
+    {
+        behaviour = Behaviour.SUCCESS;
+        t = 0;
+        remainingMoveDistance = system.arrowGap + system.arrowSize;
+        moveTarget = transform.position + new Vector3(-remainingMoveDistance, 0);
+    }
+
+    public void ActMoveOver()
+    {
+        behaviour = Behaviour.MOVE_OVER;
+        t = 0;
+        remainingMoveDistance = system.arrowGap + system.arrowSize;
+        moveTarget = transform.position + new Vector3(-remainingMoveDistance, 0);
+    }
+
+    public void ActFadeAway()
+    {
+        behaviour = Behaviour.FADE_AWAY;
+        t = 0;
+    }
+
+    void Update()
+    {
+        switch (behaviour)
+        {
+            case Behaviour.NONE:
+                {
+                    // nothing to do
+                    break;
+                }
+
+            case Behaviour.ERROR:
+                {
+                    t += Time.deltaTime;
+                    float s = 1 / errorDuration;
+                    float v = s * t;
+
+                    if (t >= errorDuration)
+                    {
+                        behaviour = Behaviour.NONE;
+                        system.Unbusy();
+                        image.transform.localScale = new Vector3(1, 1, 1);
+                        image.color = new Color(1, 1, 1, 1);
+                        break;
+                    }
+                    else if (t >= errorDuration / 2)
+                    {
+                        v = 1 - s * t;
+                    }
+
+                    float color = 1 - v;
+                    image.color = new Color(1, color, color, 1);
+
+                    float scale = 1 + errorSize * v;
+                    image.transform.localScale = new Vector3(scale, scale, 1);
+
+                    break;
+                }
+
+            case Behaviour.SUCCESS:
+                {
+                    t += Time.deltaTime;
+                    float s = 1 / successDuration;
+                    float v = s * t;
+
+                    if (t >= successDuration)
+                    {
+                        behaviour = Behaviour.NONE;
+                        system.Unbusy();
+                        image.transform.localScale = new Vector3(1, 1, 1);
+                        image.color = new Color(1, 1, 1, 1);
+                        transform.position = moveTarget;
+                        break;
+                    }
+                    else if (t >= successDuration / 2)
+                    {
+                        v = 1 - s * t;
+                    }
+
+                    float color = 1 - v;
+                    image.color = new Color(color, 1, color, 1);
+
+                    float scale = 1 + successSize * v;
+                    image.transform.localScale = new Vector3(scale, scale, 1);
+
+                    float stepSize = remainingMoveDistance * successDuration / 2f;
+                    transform.position += new Vector3(-stepSize, 0);
+
+                    break;
+                }
+
+            case Behaviour.MOVE_OVER:
+                {
+                    t += Time.deltaTime;
+                    float s = 1 / successDuration;
+
+                    if (t >= successDuration)
+                    {
+                        behaviour = Behaviour.NONE;
+                        transform.position = moveTarget;
+                        break;
+                    }
+
+                    float stepSize = remainingMoveDistance * successDuration / 2f;
+                    transform.position += new Vector3(-stepSize, 0);
+
+                    break;
+                }
+
+            case Behaviour.FADE_AWAY:
+                {
+                    t += Time.deltaTime;
+                    float s = 1 / fadeAwayDuration;
+                    float v = s * t;
+
+                    if (t >= fadeAwayDuration)
+                    {
+                        Destroy(this);
+                        break;
+                    }
+
+                    float color = 1 - v;
+                    image.color = new Color(1, 1, 1, color);
+
+                    float scale = 1 - v;
+                    image.transform.localScale = new Vector3(scale, scale, 1);
+
+                    break;
+                }
+        }
+    }
+
+    private enum Behaviour
+    {
+        NONE,
+        ERROR,
+        SUCCESS,
+        MOVE_OVER,
+        FADE_AWAY
+    }
+
+}
---
+++ Assets/Code/BattleArrowKeySystem.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleArrowKeySystem : MonoBehaviour
+{
+    public Sprite arrowLeftSprite;
+    public Sprite arrowRightSprite;
+    public Sprite arrowDownSprite;
+    public Sprite arrowUpSprite;
+
+    public float errorDuration;
+    public float errorSize;
+    public float successDuration;
+    public float successSize;
+    public float fadeAwayDuration;
+
+    public Vector2 arrowPosition;
+    public float arrowSize;
+    public float arrowGap;
+    public List<char> keys = new List<char>();
+
+    private List<GameObject> arrowEntities = new List<GameObject>();
+    private int currentKeyIndex;
+    private float startTime;
+    private bool busy;
+    private bool finished;
+
+    public void Begin()
+    {
+        // reset state
+        currentKeyIndex = 0;
+        startTime = Time.time;
+        busy = false;
+        finished = false;
+
+        // cleanup previous entities
+        foreach (GameObject go in arrowEntities)
+        {
+            Destroy(go.gameObject);
+        }
+        arrowEntities.Clear();
+
+        // generate new entities
+        Canvas canvas = FindObjectOfType<Canvas>();
+
+        for (int i = 0; i < keys.Count; i++)
+        {
+            char key = keys[i];
+
+            GameObject go = Instantiate(Resources.Load("Prefabs/ArrowEntityPrefab")) as GameObject;
+            Image image = go.GetComponentInChildren<Image>();
+            ArrowEntityBehaviour behaviour = go.GetComponent<ArrowEntityBehaviour>();
+
+            image.rectTransform.sizeDelta = new Vector2(arrowSize, arrowSize);
+            go.transform.parent = canvas.transform;
+            go.transform.position = new Vector3(arrowPosition.x + (arrowSize + arrowGap) * i, arrowPosition.y);
+            behaviour.image = image;
+            behaviour.system = this;
+
+            behaviour.errorDuration = errorDuration;
+            behaviour.errorSize = errorSize;
+            behaviour.successDuration = successDuration;
+            behaviour.successSize = successSize;
+            behaviour.fadeAwayDuration = fadeAwayDuration;
+
+            arrowEntities.Add(go);
+
+            switch (key)
+            {
+                case '<':
+                    {
+                        image.sprite = arrowLeftSprite;
+                        break;
+                    }
+                case '>':
+                    {
+                        image.sprite = arrowRightSprite;
+                        break;
+                    }
+                case 'v':
+                    {
+                        image.sprite = arrowDownSprite;
+                        break;
+                    }
+                case '^':
+                    {
+                        image.sprite = arrowUpSprite;
+                        break;
+                    }
+            }
+        }
+
+        Debug.Log("[BattleArrowKeySystem] Beginning new round.");
+        Debug.Log("[BattleArrowKeySystem] startTime = " + startTime);
+    }
+
+    public void End()
+    {
+        finished = true;
+        float timePassed = Time.time - startTime;
+
+        Debug.Log("[BattleArrowKeySystem] Ending round.");
+        Debug.Log("[BattleArrowKeySystem] ~> Time passed: " + timePassed);
+    }
+
+    public void Step(char key)
+    {
+        if (busy || finished)
+        {
+            return;
+        }
+
+        busy = true;
+
+        char currentKey = keys[currentKeyIndex];
+        bool success = key == currentKey;
+
+        Debug.Log("[BattleArrowKeySystem] Attempting a step");
+        Debug.Log("[BattleArrowKeySystem] ~> Provided key: " + key);
+        Debug.Log("[BattleArrowKeySystem] ~> Required key: " + currentKey);
+        Debug.Log("[BattleArrowKeySystem] ~> Successful? " + (success ? "Yes!" : "No."));
+
+        if (success)
+        {
+            int actorIndex = currentKeyIndex;
+
+            currentKeyIndex++;
+            if (currentKeyIndex == keys.Count)
+            {
+                End();
+            }
+
+            for (int i = 0; i < arrowEntities.Count; i++)
+            {
+                GameObject go = arrowEntities[i];
+                ArrowEntityBehaviour behaviour = go.GetComponent<ArrowEntityBehaviour>();
+
+                if (i == actorIndex)
+                {
+                    behaviour.ActSuccess();
+                }
+                else
+                {
+                    behaviour.ActMoveOver();
+                }
+            }
+        }
+        else
+        {
+            GameObject go = arrowEntities[currentKeyIndex];
+            ArrowEntityBehaviour behaviour = go.GetComponent<ArrowEntityBehaviour>();
+
+            behaviour.ActError();
+        }
+    }
+
+    public void Unbusy()
+    {
+        Debug.Log("[BattleArrowKeySystem] Unbusy() !");
+        busy = false;
+
+        if (finished)
+        {
+            Debug.Log("[BattleArrowKeySystem] finished!");
+
+            for (int i = 0; i < arrowEntities.Count; i++)
+            {
+                GameObject go = arrowEntities[i];
+                ArrowEntityBehaviour behaviour = go.GetComponent<ArrowEntityBehaviour>();
+
+                behaviour.ActFadeAway();
+            }
+        }
+    }
+
+    // FOR TESTING ONLY
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            Begin();
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            Step('^');
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            Step('v');
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            Step('<');
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            Step('>');
+        }
+    }
+
+    // FOR TESTING ONLY
+    void Start()
+    {
+        Begin();
+    }
+}
